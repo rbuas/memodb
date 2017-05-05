0d779b879d630692dece897bd85f1686cba33915
Rodrigo Buas <rodrigobuas@gmail.com>
2017-05-05 12:10:55 +0200

    export symbols correction

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = require("./memodb");
+module.exports.MemoRouter = require("./memorouter");
+module.exports.MemoModel = require("./models/memo");
+module.exports.WapModel = require("./models/wap");
+module.exports.QuoteModel = require("./models/quote");
